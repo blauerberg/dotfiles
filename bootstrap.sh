323698944e3bb75f02b8b928858638d875e11633
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2016-01-31 02:42:50 +0900

    [bootstrap] link .vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ ln -s $PWD/.tmux.conf ~/.tmux.conf
 ln -s $PWD/.gitconfig ~/.gitconfig
 ln -s $PWD/.gitignore_global ~/.gitignore_global
 ln -s $PWD/vim ~/.vim
+ln -s $PWD/vim/.vimrc ~/.vimrc
 
 # install neobundle
 source $PWD/vim/bootstrap.sh
