53fa951d8e401d15080c62114ed2570d5f6fd496
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2016-03-29 07:58:52 -0400

    bootstrap with prezto

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,24 @@
-#!/bin/sh
+#!/bin/zsh
 
 PWD=$(pwd)
+HOME="${ZDOTDIR:-$HOME}"
+git submodule update --init --recursive
 
-ln -s $PWD/.zshrc ~/.zshrc
-ln -s $PWD/.zshrc_unix ~/.zshrc_unix
+# install prezto
+ln -s $PWD/prezto $HOME/.zprezto
+setopt EXTENDED_GLOB
+for rcfile in $PWD/prezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" $HOME/.${rcfile:t}
+done
+
+#ln -s $PWD/.zshrc ~/.zshrc
+#ln -s $PWD/.zshrc_unix ~/.zshrc_unix
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 ln -s $PWD/.gitconfig ~/.gitconfig
 ln -s $PWD/.gitignore_global ~/.gitignore_global
 ln -s $PWD/vim ~/.vim
 ln -s $PWD/vim/.vimrc ~/.vimrc
+
 
 # install neobundle
 source $PWD/vim/bootstrap.sh
