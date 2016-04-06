3fc31c6c0ded69048848e3b2976692e0e3bdf955
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2016-04-07 00:04:31 +0900

    bootstrap: create symlink forcibly

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,19 +5,20 @@ HOME="${ZDOTDIR:-$HOME}"
 git submodule update --init --recursive
 
 # install prezto
-ln -s $PWD/prezto $HOME/.zprezto
+ln -fs $PWD/prezto $HOME/.zprezto
 setopt EXTENDED_GLOB
 for rcfile in $PWD/prezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" $HOME/.${rcfile:t}
+  ln -fs "$rcfile" $HOME/.${rcfile:t}
 done
 
+ln -fs $PWD/prezto-override/zpreztorc $HOME/.zpreztorc
 #ln -s $PWD/.zshrc ~/.zshrc
 #ln -s $PWD/.zshrc_unix ~/.zshrc_unix
-ln -s $PWD/.tmux.conf ~/.tmux.conf
-ln -s $PWD/.gitconfig ~/.gitconfig
-ln -s $PWD/.gitignore_global ~/.gitignore_global
-ln -s $PWD/vim ~/.vim
-ln -s $PWD/vim/.vimrc ~/.vimrc
+ln -fs $PWD/.tmux.conf ~/.tmux.conf
+ln -fs $PWD/.gitconfig ~/.gitconfig
+ln -fs $PWD/.gitignore_global ~/.gitignore_global
+ln -fs $PWD/vim ~/.vim
+ln -fs $PWD/vim/.vimrc ~/.vimrc
 
 
 # install neobundle
