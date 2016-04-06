85362272d26b5caa2ec37a1b29da4823d205883f
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2016-04-07 00:34:49 +0900

    bootstrap: fix bug that was creating unnecessary link.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,19 +5,19 @@ HOME="${ZDOTDIR:-$HOME}"
 git submodule update --init --recursive
 
 # install prezto
-ln -fs $PWD/prezto $HOME/.zprezto
+ln -fsn $PWD/prezto $HOME/.zprezto
 setopt EXTENDED_GLOB
 for rcfile in $PWD/prezto/runcoms/^README.md(.N); do
-  ln -fs "$rcfile" $HOME/.${rcfile:t}
+  ln -fsn "$rcfile" $HOME/.${rcfile:t}
 done
 
-ln -fs $PWD/prezto-override/zpreztorc $HOME/.zpreztorc
+ln -fsn $PWD/prezto-override/zpreztorc $HOME/.zpreztorc
 #ln -s $PWD/.zshrc_unix ~/.zshrc_unix
-ln -fs $PWD/.tmux.conf ~/.tmux.conf
-ln -fs $PWD/.gitconfig ~/.gitconfig
-ln -fs $PWD/.gitignore_global ~/.gitignore_global
-ln -fs $PWD/vim ~/.vim
-ln -fs $PWD/vim/.vimrc ~/.vimrc
+ln -fsn $PWD/.tmux.conf ~/.tmux.conf
+ln -fsn $PWD/.gitconfig ~/.gitconfig
+ln -fsn $PWD/.gitignore_global ~/.gitignore_global
+ln -fsn $PWD/vim ~/.vim
+ln -fsn $PWD/vim/.vimrc ~/.vimrc
 
 # install neobundle
 source $PWD/vim/bootstrap.sh
