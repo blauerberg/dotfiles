002a91e1e6cdd4bcc489d82eb8fbb67738b83e05
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2016-10-31 21:11:35 +0900

    vim: create colors dir on bootstrap

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -5,6 +5,11 @@ if [ ! -d "~/.vim/bundle/neobundle.vim" ]; then
   curl -s https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh|sh
 fi
 
+if [ ! -d "~/.vim/colors" ]; then
+  mkdir ~/.vim/colors
+fi
+
 ln -fsn ~/.vim/external/colors/vim-colors-japanesque/colors/japanesque.vim ~/.vim/colors/japanesque.vim
+
 ln -fsn ~/.vim/external/colors/vim-atom-dark/colors/atom-dark.vim ~/.vim/colors/atom-dark.vim
 ln -fsn ~/.vim/external/colors/vim-atom-dark/colors/atom-dark-256.vim ~/.vim/colors/atom-dark-256.vim
