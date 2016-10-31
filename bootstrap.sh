09e3008367ba29a53686a0da2c19d0c2468ad892
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2016-10-31 14:59:37 +0900

    move tmux.conf path

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ done
 
 ln -fsn $PWD/prezto-override/zpreztorc $HOME/.zpreztorc
 ln -fsn $PWD/prezto-override/zshrc_unix $HOME/.zshrc_unix
-ln -fsn $PWD/.tmux.conf ~/.tmux.conf
+ln -fsn $PWD/tmux/tmux.conf ~/.tmux.conf
 ln -fsn $PWD/git/gitconfig ~/.gitconfig
 ln -fsn $PWD/git/gitignore_global ~/.gitignore_global
 ln -fsn $PWD/vim ~/.vim
