32c2c237e24070d19d9a941bdb4d1f78b1548d75
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2016-11-04 23:29:23 +0900

    add .curlrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,6 +16,7 @@ ln -fsn $PWD/prezto-override/zshrc_unix $HOME/.zshrc_unix
 ln -fsn $PWD/tmux/tmux.conf ~/.tmux.conf
 ln -fsn $PWD/git/gitconfig ~/.gitconfig
 ln -fsn $PWD/git/gitignore_global ~/.gitignore_global
+ln -fsn $PWD/.curlrc ~/.curlrc
 ln -fsn $PWD/vim ~/.vim
 ln -fsn $PWD/vim/.vimrc ~/.vimrc
 
