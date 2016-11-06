7cef38d79bded660a0ba0d9985bd9a3bac308e01
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2016-11-06 19:58:54 +0900

    add .gemrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,7 @@ ln -fsn $PWD/tmux/tmux.conf ~/.tmux.conf
 ln -fsn $PWD/git/gitconfig ~/.gitconfig
 ln -fsn $PWD/git/gitignore_global ~/.gitignore_global
 ln -fsn $PWD/.curlrc ~/.curlrc
+ln -fsn $PWD/.gemrc ~/.gemrc
 ln -fsn $PWD/vim ~/.vim
 ln -fsn $PWD/vim/.vimrc ~/.vimrc
 
