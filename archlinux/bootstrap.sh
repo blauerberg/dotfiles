ee1f60c7bfa3917c0009bdd5567719500df6613b
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2019-11-19 20:29:58 +0900

    fix typo of install script for archlinux..

--- archlinux/bootstrap.sh
+++ archlinux/bootstrap.sh
@@ -3,5 +3,5 @@
 PWD=$(pwd)
 
 sudo ln -fsn $PWD/usr/lib/i3blocks/memory /usr/lib/i3blocks/memory
-sudo ln -fsn $PWD/etc/ppp/if-up.d/10-mtu.sh /etc/ppp/if-up.d/10-mtu.sh
+sudo ln -fsn $PWD/etc/ppp/ip-up.d/10-mtu.sh /etc/ppp/ip-up.d/10-mtu.sh
 sudo ln -fsn $PWD/usr/share/pulseaudio/alsa-mixer/paths/analog-input-internal-mic.conf /usr/share/pulseaudio/alsa-mixer/paths/analog-input-internal-mic.conf
