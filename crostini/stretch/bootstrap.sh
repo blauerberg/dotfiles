4a6ce571c01a40aee90605fab0f0046ceb6221a6
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2020-02-03 16:47:22 +0900

    assue "yes" for apt-get

--- crostini/stretch/bootstrap.sh
+++ crostini/stretch/bootstrap.sh
@@ -18,15 +18,15 @@ sudo apt-get -y install $PACKAGES
 # install vscode
 # see: https://code.visualstudio.com/docs/setup/linux
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo install -y -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https
+sudo apt-get -y install apt-transport-https
 sudo apt-get update
-sudo apt-get install code
+sudo apt-get -y install code
 
 # install Docke CE
 # see: https://docs.docker.com/install/linux/docker-ce/debian/
-sudo apt-get install \
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -38,7 +38,7 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker ${USER}
 sudo systemctl enable docker
 sudo systemctl restart docker
