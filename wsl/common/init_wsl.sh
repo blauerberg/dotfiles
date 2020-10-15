00b38102768516e5bad15d58ceb94e7a2e4792b7
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2020-10-15 17:46:16 +0900

    add distro-independet initialize scripts for wsl

---
+++ wsl/common/init_wsl.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# create /etc/wsl.conf
+if [ ! -e "/etc/wsl.conf" ]; then
+  sudo cp wsl.conf /etc/wsl.conf
+fi
+
+# symbolic link for Windows Desktop
+ln -fs "/mnt/c/Users/$(whoami)/Desktop" ~/Desktop
+
+# symbolic link for vscode
+VSCODE_BIN="/mnt/c/Users/$(whoami)/AppData/Local/Programs/Microsoft VS Code/bin/code"
+if [ -e "$VSCODE_BIN" ]; then
+  sudo ln -fs "$VSCODE_BIN" /usr/local/bin/code
+fi
