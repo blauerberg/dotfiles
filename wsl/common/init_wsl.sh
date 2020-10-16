b380a5b484aef2a5f46fdebb7f05c340688e904e
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2020-10-16 13:34:09 +0900

    add pbcopy alias for wsl

--- wsl/common/init_wsl.sh
+++ wsl/common/init_wsl.sh
@@ -13,3 +13,6 @@ VSCODE_BIN="/mnt/c/Users/$(whoami)/AppData/Local/Programs/Microsoft VS Code/bin/
 if [ -e "$VSCODE_BIN" ]; then
   sudo ln -fs "$VSCODE_BIN" /usr/local/bin/code
 fi
+
+# symbolic link for clip.exe
+sudo ln -fs  "/mnt/c/Windows/system32/clip.exe" /usr/local/bin/clip
