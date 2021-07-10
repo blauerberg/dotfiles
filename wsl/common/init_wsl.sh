f98cf00a147492ebc1e7a2c7cb9dd1c146d30ce6
Yoshikazu Aoyama <yskz.aoyama@gmail.com>
2021-07-10 19:09:47 +0900

    wsl: cosmetic changes

--- wsl/common/init_wsl.sh
+++ wsl/common/init_wsl.sh
@@ -5,23 +5,30 @@ if [ ! -e "/etc/wsl.conf" ]; then
   sudo cp wsl.conf /etc/wsl.conf
 fi
 
-# symbolic link for Windows Desktop
-ln -fs "/mnt/c/Users/$(whoami)/Desktop" ~/Desktop
+# create symbolic links.
+# Windows Desktop
+ln -fsn "/mnt/c/Users/$(whoami)/Desktop" ~/Desktop
 
-# symbolic link for vscode
+# Windows Downloads dir
+ln -fsn "/mnt/c/Users/$(whoami)/Downloads" ~/Downloads
+
+# clip.exe
+sudo ln -fsn "/mnt/c/Windows/system32/clip.exe" /usr/local/bin/clip
+
+# Command Prompt
+sudo ln -fsn /mnt/c/Windows/system32/cmd.exe /usr/local/bin/cmd.exe
+
+# PowerShell
+sudo ln -fsn /mnt/c/Windows/system32/WindowsPowerShell/v1.0/powershell.exe /usr/local/bin/powershell.exe
+
+# vscode
 VSCODE_BIN="/mnt/c/Users/$(whoami)/AppData/Local/Programs/Microsoft VS Code/bin/code"
 if [ -e "$VSCODE_BIN" ]; then
-  sudo ln -fs "$VSCODE_BIN" /usr/local/bin/code
+  sudo ln -fsn "$VSCODE_BIN" /usr/local/bin/code
 fi
 
-# symbolic link for clip.exe
-sudo ln -fs  "/mnt/c/Windows/system32/clip.exe" /usr/local/bin/clip
-
-# symbolic link for Docker Desktop
-sudo ln -fs "/mnt/c/Program Files/Docker/Docker/resources/bin/docker-credential-desktop.exe" /usr/local/bin/docker-credential-desktop.exe
-
-# symbolic link for cmd.exe
-sudo ln -fs /mnt/c/Windows/system32/cmd.exe /usr/local/bin/cmd.exe
-
-# symbolic link for powershell.exe
-sudo ln -fs /mnt/c/Windows/system32/WindowsPowerShell/v1.0/powershell.exe /usr/local/bin/powershell.exe
+# Docker Desktop
+DOCKER_DESKTOP_BIN="/mnt/c/Program Files/Docker/Docker/resources/bin/docker-credential-desktop.exe"
+if [ -e "$DOCKER_DESKTOP_BIN" ]; then
+  sudo ln -fsn "$DOCKER_DESKTOP_BIN" /usr/local/bin/docker-credential-desktop.exe
+fi
