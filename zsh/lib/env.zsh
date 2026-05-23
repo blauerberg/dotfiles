f0b403545a9645a4d1ec4a5954bb23d31763a0f4
Yoshikazu Aoyama <175681+blauerberg@users.noreply.github.com>
2026-05-24 00:16:31 +0900

    chore(zsh): tune history noise filters

--- zsh/lib/env.zsh
+++ zsh/lib/env.zsh
@@ -1,6 +1,8 @@
 setopt interactivecomments
 setopt nonomatch
 setopt hist_ignore_dups
+setopt hist_ignore_all_dups
+setopt hist_expire_dups_first
 setopt hist_ignore_space
 setopt hist_reduce_blanks
 setopt inc_append_history
@@ -9,7 +11,7 @@ setopt share_history
 export HISTFILE="${HISTFILE:-$HOME/.zsh_history}"
 export HISTSIZE="${HISTSIZE:-1000000}"
 export SAVEHIST="${SAVEHIST:-1000000}"
-export HISTORY_IGNORE="${HISTORY_IGNORE:-"(cd|pwd|exit|l[sal]|ls -*)"}"
+export HISTORY_IGNORE="${HISTORY_IGNORE:-"(ls|ls -al|ls -ltr|date|pwd|exit|clear|git log|git log -p|git status|git diff|git lg)"}"
 
 typeset -U path PATH
 
