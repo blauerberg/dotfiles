76aaff04a1a8d008b0add76bc896b0ee270a20ab
Yoshikazu Aoyama <175681+blauerberg@users.noreply.github.com>
2026-05-26 16:17:27 +0900

    feat(nvim): close diffview with <leader>gq

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -22,6 +22,7 @@ return {
       { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'Diff view' },
       { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = 'File history' },
       { '<leader>gH', '<cmd>DiffviewFileHistory<cr>', desc = 'Repo history' },
+      { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = 'Close diff view' },
     },
     opts = {},
   },
