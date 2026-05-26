b5c048330e29a6b5475ebad0e3aad735737ad6a9
Yoshikazu Aoyama <175681+blauerberg@users.noreply.github.com>
2026-05-26 15:45:25 +0900

    feat(nvim): add nvim-treesitter (master branch)

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
+  build = ':TSUpdate',
+  event = { 'BufReadPost', 'BufNewFile' },
+  main = 'nvim-treesitter.configs',
+  opts = {
+    ensure_installed = {
+      'typescript', 'tsx', 'javascript', 'python',
+      'lua', 'vim', 'vimdoc', 'json', 'yaml', 'bash', 'markdown',
+    },
+    auto_install = false,
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+}
